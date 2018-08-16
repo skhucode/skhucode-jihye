53d7978fb78a6ee7299010b92e618fe233946a82
KimJye <eoeogudgud@naver.com>
2018-08-17 02:54:04 +0900

    동적프로그래밍

--- dp/붕어빵판매_p11052.java
+++ dp/붕어빵판매_p11052.java
@@ -27,3 +27,45 @@ public class ºØ¾î»§ÆÇ¸Å_p11052 {
 	}
 
 }
+
+
+/* Àç±Í·Î Ç®±â
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n= Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int[] arr = new int[n+1];
+        int i=1;
+        while(st.hasMoreTokens()){
+            arr[i++]=Integer.parseInt(st.nextToken());
+        }
+        
+        System.out.println(f(arr,n));
+    }
+    
+    public static int f(int[] arr, int n){
+        int[] dp = new int[n+1];
+        dp[1]=arr[1];
+        
+        for(int i=2; i<=n; ++i){
+            for(int j=1; j<=n; ++j){
+                if(i-j>=0)
+                dp[i]=Math.max(dp[i],dp[i-j]+arr[j]);
+            }
+        }
+        return dp[n];
+    }
+}
+
+
+
+*/
